351d2e0a0b790531beff4a7b97fce9e95d1de8c4
xuehuiyun <114709062+xuehuiyun@users.noreply.github.com>
2023-12-19 00:11:40 -0800

    Add files via upload

---
+++ movie/src/main/java/evaxuexue/dev/movie/MovieController.java
@@ -0,0 +1,29 @@
+package evaxuexue.dev.movie;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+@CrossOrigin(origins = "http://localhost:3000")
+public class MovieController {
+    @Autowired
+    private MovieService service;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getMovies() {
+        return new ResponseEntity<List<Movie>>(service.findAllMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId){
+        return new ResponseEntity<Optional<Movie>>(service.findMovieByImdbId(imdbId), HttpStatus.OK);
+    }
+}
